60f846b2aebcf9860db5e7386fda03db986dd526
兰桑 <l1033182818j@gmail.com>
2019-12-04 11:19:58 +0800

    修改导航

--- src/views/controller-one/controllerOne.module.ts
+++ src/views/controller-one/controllerOne.module.ts
@@ -2,5 +2,10 @@ export default [
     
     {path:"home-page",component:()=>import("./home-page/homePage.vue")},
 
-    {path:"sample-page",component:()=>import("./sample-page/samplePage.vue")}
+    {path:"sample-page",component:()=>import("./sample-page/samplePage.vue")},
+
+    {path:"sample2-page",component:()=>import("./sample2-page/samplePage.vue")},
+
+    {path:"sample3-page",component:()=>import("./sample3-page/samplePage.vue")},
+
 ];
--- src/views/navData.ts
+++ src/views/navData.ts
@@ -5,15 +5,21 @@ export {
   NavListItem
 }
 
+// export const navData:NavListItem[]= [
+//   {path:"/controller/home-page",label:"首页",icon:"account"},
+//   {label:"数据探索",icon:"explore",children:[
+//     {path:"/controller/sample-page",label:"sample"},
+//   ]},
+//   {label:"数据建模",icon:'model',children:[
+//     {path:"/controller/sample-page",label:"sample"},
+//   ]},
+//   {label:"产品管理",icon:'manage',children:[
+//     {path:"/controller/samplage-page",label:"sample"},
+//   ]}
+// ];
 export const navData:NavListItem[]= [
   {path:"/controller/home-page",label:"首页",icon:"account"},
-  {label:"数据探索",icon:"explore",children:[
-    {path:"/controller/sample-page",label:"sample"},
-  ]},
-  {label:"数据建模",icon:'model',children:[
-    {path:"/controller/sample-page",label:"sample"},
-  ]},
-  {label:"产品管理",icon:'manage',children:[
-    {path:"/controller/samplage-page",label:"sample"},
-  ]}
+  {path:"/controller/sample-page",label:"数据探索",icon:"explore",children:[]},
+  {path:"/controller/sample2-page",label:"数据建模",icon:'model',children:[]},
+  {path:"/controller/sample3-page",label:"产品管理",icon:'manage',children:[]}
 ];
