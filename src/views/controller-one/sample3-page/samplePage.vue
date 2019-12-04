26824e11b98e37046afc8492a2b3dd5296ff76b9
兰桑 <l1033182818j@gmail.com>
2019-12-04 11:18:08 +0800

    daohang

---
+++ src/views/controller-one/sample3-page/samplePage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>sample3</div>
+</template>
+
+<script lang="ts">
+import {Component,Vue} from "vue-property-decorator";
+
+@Component({})
+export default class extends Vue{
+
+}
+</script>
+<style>
+
+</style>
